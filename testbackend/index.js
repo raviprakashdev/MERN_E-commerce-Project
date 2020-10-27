4c5e25f2a125e08d15e0a491a32fde20405df6ae
Ravi Prakash <41155449+raviprakashdev@users.noreply.github.com>
2020-10-28 00:55:17 +0530

    day 2

--- testbackend/index.js
+++ testbackend/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Hello Ravi Prakash !!!"))
+app.get("/", (req, res) => {
+  return res.send("Hello Ravi Prakash  !!!");
+});
 
-app.listen(port, () => console.log('port no. ${port}'))
+app.listen(port, () => console.log(`port no. ${port}`));
